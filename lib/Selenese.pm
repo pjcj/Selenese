e421096c445bfd09b68dbc9bcf203e74f1aef06f
Paul Johnson <paul.johnson@tech.swisssign.com>
2016-09-08 16:24:23 +0200

    Add UTF-8, logging, functionality, error handling.

--- lib/Selenese.pm
+++ lib/Selenese.pm
@@ -6,10 +6,14 @@ package Selenese;
 
 use 5.18.2;
 use warnings;
+use utf8::all;
 
 use Data::Dumper;
+use Encode;
 use JavaScript::V8;
 use Test::More;
+use Time::HiRes qw( usleep );
+use Unicode::Normalize qw( NFC NFD NFKC NFKD );
 
 $Data::Dumper::Indent   = 1;
 $Data::Dumper::Purity   = 1;
@@ -23,8 +27,15 @@ sub new {
     $self
 }
 
-sub verbose { shift->{verbose}                             }
-sub driver  { shift->{driver} // die "No driver specified" }
+sub verbose       { shift->{verbose}                             }
+sub driver        { shift->{driver} // die "No driver specified" }
+sub d             { shift->{d}                                   }
+sub error_capture { shift->{error_capture}                       }
+sub error_html    { shift->{error_html}                          }
+sub debug {
+    my $self = shift;
+    $self->{debug_sub} //= sub { say "@_" if $self->{debug} };
+}
 
 sub parse_loc {
     my $self = shift;
@@ -36,6 +47,12 @@ sub parse_loc_with_default {
     my $self = shift;
     my ($loc) = @_;
     $loc =~ /^(\w+)=(.*)/ ? ($1, $2) : ("xpath", $loc)
+}
+
+sub text_to_re {
+    my $self = shift;
+    my ($text) = @_;
+    $text =~ /^regexp:(.*)/ ? qr/$1/ : qr/\Q$text/
 }
 
 sub init {
@@ -95,7 +112,7 @@ sub init {
                 $d->get($loc);
             }
         },
-        clickAndWait => sub {
+        click => sub {
             my ($loc) = @_;
             if (my ($scheme, $target) = $self->parse_loc_with_default($loc)) {
                 $d->find_element($target, $scheme)->click;
@@ -117,31 +134,42 @@ sub init {
             my ($select, $option) = @_;
             my ($sid, $s) = $self->parse_loc_with_default($select);
             my ($oid, $o) = $self->parse_loc_with_default($option);
-            if (grep defined, $sid, $s, $oid, $o) {
-                my $opt = ($oid eq "label" ? "normalize-space(.)" : "\@$oid");
-                if (my ($re) = $o =~ /^regexp:(.*)/) {
-                    $re = qr|$re|;
-                    my $f = "//select[\@$sid='$s']";
-                    $o = "";
-                    my $select = $d->find_element($f);
-                    my @children = $d->find_child_elements($select, "./option");
+            die "Can't handle", $select, $option
+                unless grep defined, $sid, $s, $oid, $o;
+            my $opt = ($oid eq "label" ? "normalize-space(.)" : "\@$oid");
+            $self->debug->(
+                select => [$select, $option, $sid, $s, $oid, $o, $opt]);
+            if (my ($re) = $o =~ /^regexp:(.*)/) {
+                $re          = qr|$re|;
+                my $f        = "//select[\@$sid='$s']";
+                $o           = "";
+                my $sel      = $d->find_element($f);
+                my @children = $d->find_child_elements($sel, "./option");
+                $self->debug->(children => $f, \@children);
+                GET: for my $get (qw( get_tag_name get_value )) {
                     for my $e (@children) {
                         my $val = ($oid eq "label") ? $e->get_text
-                                                    : $e->get_tag_name;
-                        next unless $val =~ $re;
+                                                    : $e->$get;
+                        $self->debug->(select => [$get, $e, $val, $re]);
+                        next unless $val =~ /$re/;
                         $o = $val;
                         s/^\s+//, s/\s+$//, s/\s+/ / for $o;
-                        last;
+                        last GET;
                     }
-                    die "Can't find element matching", "[$re]" unless $o;
                 }
-                $opt .= "='$o'";
-                my $f = "//select[\@$sid='$s']/option[$opt]";
-                my $e = $d->find_element($f);
-                $e->click;
-            } else {
-                die "Can't handle", $select, $option;
+                die "Can't find element matching", "[$re]" unless $o;
             }
+            my $f;
+            if ($oid eq "index") {
+                $o++;  # XPath indices start at 1!
+                $f = "(//select[\@$sid='$s']/option)[$o]";
+            } else {
+                $opt .= "='$o'";
+                $f = "//select[\@$sid='$s']/option[$opt]";
+            }
+            $self->debug->(find => $f);
+            my $e = $d->find_element($f);
+            $e->click;
         },
         setTimeout => sub {
             my ($ms) = @_;
@@ -150,6 +178,30 @@ sub init {
             $d->set_timeout("implicit",  $ms);
             $d->set_timeout("page load", $ms);
         },
+        verifyTextPresent => sub {
+            my ($text) = @_;
+            my $re     = $self->text_to_re($text);
+            my $body   = $d->get_body;
+            $body =~ $re
+        },
+        verifyTextNotPresent => sub {
+            !$self->{cmds}{verifyTextPresent}->(@_)
+        },
+        verifyText => sub {
+            my ($select, $text) = @_;
+            $self->debug->(verifyText => $select, $text);
+            my ($sid, $s) = $self->parse_loc_with_default($select);
+            die "Can't handle", $select, $text unless grep defined, $sid, $s;
+            my $re  = $self->text_to_re($text);
+            my $sel = $d->find_element($s);
+            my $txt = $sel->get_text;
+            $self->debug->("verifyText match" => $txt, $re);
+            $txt =~ $re
+        },
+        pause => sub {
+            my ($duration) = @_;
+            usleep($duration * 1000);
+        },
         deleteAllVisibleCookies => sub {
             $d->delete_all_cookies;
         },
@@ -157,11 +209,41 @@ sub init {
         },
         quit => sub {
             $d->quit;
+            system "pgrep phantomjs 2> /dev/null | xargs kill 2> /dev/null";
         },
     };
 
-    $cmds->{click} = $cmds->{clickAndWait};
-    $cmds->{addSelection} = $cmds->{select};
+    my $equalities = {
+        clickAndWait      => "click",
+        selectAndWait     => "select",
+        addSelection      => "select",
+        verifyBodyText    => "verifyTextPresent",
+        verifyNotBodyText => "verifyTextNotPresent",
+    };
+    while (my ($to, $from) = each %$equalities) {
+        $cmds->{$to} = $cmds->{$from};
+    }
+}
+
+sub capture {
+    my $self = shift;
+    my ($filename) = @_;
+    die "No filename for capture" unless $filename;  # no, don't use "0"!
+    $self->d->maximize_window;
+    $self->d->capture_screenshot($filename);
+    say "Screen capture written to $filename";
+}
+
+sub write_page_source {
+    my $self = shift;
+    my ($filename) = @_;
+    die "No filename for capture" unless $filename;  # no, don't use "0"!
+    my $out = $self->d->get_page_source;
+    open my $fh, ">", $filename or die "Can't open $filename: $!";
+    binmode $fh, ":encoding(UTF-8)";
+    print $fh $out;
+    close $fh or die "Can't close $filename: $!";
+    say "Page source written to $filename";
 }
 
 sub run_command {
@@ -174,12 +256,27 @@ sub run_command {
     my $ret = eval { $sub->(@params) };
     my $err = $@;
     ok !$err, "$cmd @params";
-    BAIL_OUT("Exiting") if $err;
+    if ($err) {
+        $self->capture($self->error_capture)        if $self->error_capture;
+        $self->write_page_source($self->error_html) if $self->error_html;
+        $self->{cmds}{quit}->();
+        BAIL_OUT("Exiting: $err");
+    }
+
+    $self->{post_sub}->(
+        selenese => $self,
+        ret      => $ret,
+        err      => $err,
+        cmd      => $cmd,
+        params   => \@params,
+    ) if $self->{post_sub};
+
     $ret
 }
 
 "
-Careful, now!
+But tonight we'll be free
+All the promises'll be broken
 "
 
 __END__
